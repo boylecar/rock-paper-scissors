af65cf2c6c1f57357efe0a0da09e535ae5594cb0
boylecar <114616620+boylecar@users.noreply.github.com>
2022-11-09 13:14:11 -0800

    console.log the amount of times rock paper scissors are printed out of 99 trials, their over/under the expected value also printed.

--- script.js
+++ script.js
@@ -8,22 +8,50 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 let r = 0;
 let p = 0;
 let s = 0;
-for (let i = 0; i<100; i++) {
-    getComputerChoice();
-    if (getComputerChoice == "rock") {
+
+for (let i = 0; i<5; i++) {
+    r++;
+    if(i == 4) {
+console.log(r);
+    }
+}
+for (let i = 0; i<98; i++) {
+    let b = getComputerChoice();
+    if (b == "rock") {
         r++;
-    } else if (getComputerChoice == "paper") {
+    } else if (b == "paper") {
         p++;
-    } else if (getComputerChoice == "scissors") {
+    } else if (b == "scissors") {
         s++;
     }
-    if (i == 98) {
+    if (i == 97) {
         console.log(r);
+        if (r >= 33) {
+            let Over = r - 33;
+            console.log("Rock appeared " + Over + " more times than expected.");
+        } else if (r < 33) {
+            let Under = 33 - r;
+            console.log("Rock appeared " + Under + " less times than expected.");
+        }
         console.log(p);
+        if (p >= 33) {
+            let Over = p - 33;
+            console.log("Paper appeared " + Over + " more times than expected.");
+        } else if (p < 33) {
+            let Under = 33 - p;
+            console.log("Paper appeared " + Under + " less times than expected.");
+        }
         console.log(s);
+        if (s >= 33) {
+            let Over = s - 33;
+            console.log("Scissors appeared " + Over + " more times than expected.");
+        } else if (s < 33) {
+            let Under = 33 - s;
+            console.log("Scissors appeared " + Under + " less times than expected.");
+        }
     }
 }
